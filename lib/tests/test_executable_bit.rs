c97ebe2e6082dae5c820ee81e6b71c567c6cdc67
Ian Wrzesinski <wrzian@umich.edu>
2025-03-18 19:22:52 -0400

    lib/tests: Add initial tests for executable bit changes

---
+++ lib/tests/test_executable_bit.rs
@@ -0,0 +1,134 @@
+use std::fs;
+#[cfg(unix)]
+use std::os::unix::fs::MetadataExt as _;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
+
+use jj_lib::backend::MergedTreeId;
+use jj_lib::backend::TreeId;
+use jj_lib::backend::TreeValue;
+use jj_lib::commit::Commit;
+use jj_lib::repo::Repo as _;
+use jj_lib::repo_path::RepoPath;
+use jj_lib::repo_path::RepoPathComponent;
+use jj_lib::tree::Tree;
+use jj_lib::working_copy::CheckoutOptions;
+use testutils::TestWorkspace;
+
+// All tests just use a single filename. Make it a global for convenience.
+const NAME: &str = "dummy-file-name";
+
+/// Flip the executable bit of a file on the filesystem.
+fn flip_file_exec_bit(path: &PathBuf) {
+    let file = fs::File::open(path).unwrap();
+    #[cfg(windows)]
+    let _ = file;
+    #[cfg(unix)]
+    {
+        let new_mode = 0o100 ^ file.metadata().unwrap().mode();
+        file.set_permissions(PermissionsExt::from_mode(new_mode))
+            .unwrap();
+        let mode = file.metadata().unwrap().mode();
+        assert_eq!(mode, new_mode); // sanity check
+    }
+}
+
+/// Returns a new tree id with the executable bit for this path flipped.
+fn flip_tree_exec_bit(tree: &Tree, path: &str) -> TreeId {
+    let repo_path_component = RepoPathComponent::new(path).unwrap();
+    let store = tree.store();
+    let mut builder = store.tree_builder(store.empty_tree_id().clone());
+    let tree_val = tree.value(repo_path_component).unwrap().clone();
+    let TreeValue::File { id, mut executable } = tree_val else {
+        panic!()
+    };
+    executable = !executable;
+    builder.set(
+        RepoPath::root().join(repo_path_component),
+        TreeValue::File { id, executable },
+    );
+    builder.write_tree().unwrap()
+}
+
+/// Assert a file on the filesystem has the expected executable bit (on Unix).
+#[track_caller]
+fn assert_file_exec_bit(path: &PathBuf, expected: bool) {
+    let file = fs::File::open(path).unwrap();
+    let perms = file.metadata().unwrap().permissions();
+    #[cfg(unix)]
+    let actual = (perms.mode() & 0o100) == 0o100;
+    #[cfg(windows)]
+    let (actual, _) = (expected, perms);
+    assert_eq!(actual, expected);
+}
+
+/// Assert a stored tree value has the expected executable bit.
+#[track_caller]
+fn assert_tree_value_exec_bit(tree_val: &TreeValue, expected: bool) {
+    match tree_val {
+        TreeValue::File { id: _, executable } => {
+            assert_eq!(*executable, expected);
+        }
+        _ => panic!(),
+    }
+}
+
+/// Checkout a commit with the given checkout options.
+fn checkout_with_opts(ws: &TestWorkspace, commit: &Commit, options: &CheckoutOptions) {
+    let mut locked_wc = ws.workspace.working_copy().start_mutation().unwrap();
+    locked_wc.check_out(commit, options).unwrap();
+    let op_id = ws.repo.op_id().clone();
+    locked_wc.finish(op_id).unwrap();
+}
+
+/// Test that snapshotting files stores the correct executable bit in the tree.
+#[test]
+fn test_exec_bit_snapshot() {
+    let mut ws = TestWorkspace::init();
+    let path = &ws.workspace.workspace_root().to_owned().join(NAME);
+    let repo_path_component = RepoPathComponent::new(NAME).unwrap();
+
+    // > Snapshot tree values when the file is/isn't executable.
+    fs::File::create(path).unwrap();
+    let tree = ws.snapshot().unwrap().take().into_resolved().unwrap();
+    let val = tree.value(repo_path_component).unwrap();
+    assert_tree_value_exec_bit(val, false);
+
+    flip_file_exec_bit(path);
+    let tree = ws.snapshot().unwrap().take().into_resolved().unwrap();
+    let val = tree.value(repo_path_component).unwrap();
+    assert_tree_value_exec_bit(val, cfg!(unix)); // Exec bit only stored on unix
+}
+
+/// Test that checking out a tree writes the correct executable bit to the
+/// filesystem.
+#[test]
+fn test_exec_bit_checkout() {
+    let ws = TestWorkspace::init();
+    let path = &ws.workspace.workspace_root().to_owned().join(NAME);
+    let repo_path = RepoPath::from_internal_string(NAME).unwrap();
+
+    // > Build two commits that write the executable bit as true/false.
+    let tree = testutils::create_single_tree(&ws.repo, &[(repo_path, "")]);
+    let tree_no_exec = tree.id().clone();
+    let tree_exec = flip_tree_exec_bit(&tree, NAME);
+    assert_ne!(tree_no_exec, tree_exec);
+    let commit_with_tree_id =
+        |id: TreeId| testutils::commit_with_tree(ws.repo.store(), MergedTreeId::resolved(id));
+    let commit_exec = &commit_with_tree_id(tree_exec);
+    let commit_no_exec = &commit_with_tree_id(tree_no_exec);
+    assert_ne!(commit_exec, commit_no_exec);
+
+    // > Checkout commits and ensure the filesystem is updated correctly.
+    assert!(!fs::exists(path).unwrap());
+    let options = &CheckoutOptions::empty_for_test();
+    checkout_with_opts(&ws, commit_exec, options);
+    assert_file_exec_bit(path, true);
+
+    checkout_with_opts(&ws, commit_no_exec, options);
+    assert_file_exec_bit(path, false);
+
+    checkout_with_opts(&ws, commit_exec, options);
+    assert_file_exec_bit(path, true);
+}
