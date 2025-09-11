a29adf11d3e7c1dafc1ed68c14fe7770abf73a68
Ian Wrzesinski <ian@isuffix.com>
2025-09-11 03:33:35 -0400

    lib/tests: Ignore `test_concurrent_read_write_commit` by default

--- lib/tests/test_git.rs
+++ lib/tests/test_git.rs
@@ -4311,6 +4311,7 @@ fn test_concurrent_write_commit() {
 }
 
 #[test]
+#[ignore = "this has been consistently flaky, see issue #7318"]
 fn test_concurrent_read_write_commit() {
     let settings = user_settings_without_change_id();
     let test_repo = TestRepo::init_with_backend(TestRepoBackend::Git);
