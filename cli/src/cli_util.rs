480ea56b2192978cafdb231c47732faa0ceb7b3f
Tejas Sanap <email@whereistejas.xyz>
2025-10-13 16:14:22 +0100

    cli: always check if `explicit_paths` is empty
    We should not display any warning if `explicit_paths` is empty.
    Previously, we would by-pass this check if `trees` was empty (which was
    incorrect behaviour).

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -2838,10 +2838,12 @@ pub fn print_unmatched_explicit_paths<'a>(
     for tree in trees {
         // TODO: propagate errors
         explicit_paths.retain(|&path| tree.path_value(path).unwrap().is_absent());
-        if explicit_paths.is_empty() {
-            return Ok(());
-        }
     }
+
+    if explicit_paths.is_empty() {
+        return Ok(());
+    }
+
     let ui_paths = explicit_paths
         .iter()
         .map(|&path| workspace_command.format_file_path(path))
