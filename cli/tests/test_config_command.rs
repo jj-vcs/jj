c072d4277edfd3cb29b42e9de1167ed743bedd58
David Higgs <higgsd@gmail.com>
2025-11-16 09:10:30 -0500

    tests: exercise warning about changing author

--- cli/tests/test_config_command.rs
+++ cli/tests/test_config_command.rs
@@ -1753,13 +1753,50 @@ fn test_config_author_change_warning() {
         })
         .success();
 
-    let output = work_dir.run_jj(["log"]);
+    let output = work_dir.run_jj([
+        "log",
+        "-T",
+        "separate(' ', self.commit_id().shortest(8), self.author())",
+    ]);
     insta::assert_snapshot!(output, @r"
-    @  qpvuntsm Foo 2001-02-03 08:05:09 c2090b51
-    │  (empty) (no description set)
-    ◆  zzzzzzzz root() 00000000
+    @  c2090b51 Test User <Foo>
+    ◆  00000000
     [EOF]
     ");
+
+    let output = work_dir.run_jj(["config", "set", "--repo", "user.name", "'Bar'"]);
+    insta::assert_snapshot!(output, @r#"
+    ------- stderr -------
+    Warning: This setting will only impact future commits.
+    The author of the working copy will stay "Test User <Foo>".
+    To change the working copy author, use "jj metaedit --update-author"
+    [EOF]
+    "#);
+
+    // test_env.run_jj*() resets state for every invocation
+    // for this test, both user.name and user.email are needed
+    work_dir
+        .run_jj_with(|cmd| {
+            cmd.args(["metaedit", "--update-author"])
+                .env_remove("JJ_EMAIL")
+                .env_remove("JJ_USER")
+        })
+        .success();
+
+    let output = work_dir.run_jj([
+        "log",
+        "-T",
+        "separate(' ', self.commit_id().shortest(8), self.author())",
+    ]);
+    insta::assert_snapshot!(output, @r"
+    @  cab1afa9 Bar <Foo>
+    ◆  00000000
+    [EOF]
+    ");
+
+    // no warnings if no change
+    let output = work_dir.run_jj(["config", "set", "--repo", "user.name", "'Bar'"]);
+    insta::assert_snapshot!(output, @"");
 }
 
 #[test]
