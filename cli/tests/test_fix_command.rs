e60b88cafe41389b8427dc71efee7eb0d47a7e5e
Kaiyi Li <kaiyili@google.com>
2025-11-23 09:33:44 -0800

    cli: simplify `jj fix` child process handling
    * `Some(x).unwrap` is not necessary.
    * No need to spawn a new thread.
    * The old implementation doesn't handle the case where it fails to write
      to the stdin pipe. Now it's an error returned.

--- cli/tests/test_fix_command.rs
+++ cli/tests/test_fix_command.rs
@@ -965,6 +965,32 @@ fn test_fix_empty_file() {
 }
 
 #[test]
+fn test_fix_large_file() {
+    let mut test_env = TestEnvironment::default();
+    set_up_fake_formatter(&mut test_env, &["--lowercase"]);
+    test_env.run_jj_in(".", ["git", "init", "repo"]).success();
+    let work_dir = test_env.work_dir("repo");
+    let mut large_contents = (0..0x1_000_000).map(|_| b'A').collect::<Vec<_>>();
+    large_contents.push(b'\n');
+    work_dir.write_file("file", &large_contents);
+
+    work_dir.run_jj(["config", "set", "--repo", "snapshot.max-new-file-size", "16777217"]).success();
+    let output = work_dir.run_jj(["fix", "-s", "@"]);
+    insta::assert_snapshot!(output, @r"
+    ------- stderr -------
+    Fixed 1 commits of 1 checked.
+    Working copy  (@) now at: qpvuntsm cc4f0abd (no description set)
+    Parent commit (@-)      : zzzzzzzz 00000000 (empty) (no description set)
+    Added 0 files, modified 1 files, removed 0 files
+    [EOF]
+    ");
+    let output = work_dir.run_jj(["file", "show", "file", "-r", "@"]);
+    let stdout_bytes = output.stdout.raw().as_bytes();
+    assert_eq!(stdout_bytes.len(), large_contents.len());
+    assert!(stdout_bytes == &large_contents.to_ascii_lowercase());
+}
+
+#[test]
 fn test_fix_some_paths() {
     let mut test_env = TestEnvironment::default();
     set_up_fake_formatter(&mut test_env, &["--uppercase"]);
