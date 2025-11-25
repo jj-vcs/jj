75d7d942e2c454b75b584cebb7f9725ad9540563
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2025-11-25 22:37:51 +0100

    change-id: avoid prefix collision when creating a new revision

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -2363,6 +2363,17 @@ See https://jj-vcs.github.io/jj/latest/working-copy/#stale-working-copy \
 
         Ok(advanceable_bookmarks)
     }
+
+    pub fn generate_new_change_id(&self) -> ChangeId {
+        let id_prefix_index = self
+            .id_prefix_context()
+            .populate(self.repo().base_repo())
+            .unwrap();
+        id_prefix_index.generate_new_change_id(
+            self.settings().get_rng(),
+            self.repo().store().change_id_length(),
+        )
+    }
 }
 
 #[cfg(feature = "git")]
@@ -2479,6 +2490,17 @@ impl WorkspaceCommandTransaction<'_> {
     ) -> Result<TemplateRenderer<'_, Commit>, CommandError> {
         let language = self.commit_template_language();
         self.helper.env.parse_template(ui, &language, template_text)
+    }
+
+    pub fn generate_new_change_id(&self) -> ChangeId {
+        let id_prefix_context = self
+            .id_prefix_context
+            .get_or_init(|| self.helper.env.new_id_prefix_context());
+        let id_prefix_index = id_prefix_context.populate(self.repo()).unwrap();
+        id_prefix_index.generate_new_change_id(
+            self.settings().get_rng(),
+            self.repo().store().change_id_length(),
+        )
     }
 
     pub fn finish(self, ui: &Ui, description: impl Into<String>) -> Result<(), CommandError> {
