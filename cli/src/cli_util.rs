8af7ee08a69739a43ceefb230f614a12cd44e8d7
Hubert Lefevre <hubelefevre@gmail.com>
2025-12-20 15:29:28 +0100

    cli: Have print_collasped_untracked_files take a &MergeTree
    We can work with a borrowed Tree and we do not need to clone it.

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -4558,7 +4558,7 @@ fn warn_if_args_mismatch(
 
 pub async fn visit_collapsed_untracked_files(
     untracked_paths: impl IntoIterator<Item = impl AsRef<RepoPath>>,
-    tree: MergedTree,
+    tree: &MergedTree,
     mut on_path: impl FnMut(&RepoPath, bool) -> Result<(), CommandError>,
 ) -> Result<(), CommandError> {
     let trees = tree.trees().await?;
@@ -4663,7 +4663,7 @@ mod tests {
 
     fn collect_collapsed_untracked_files_string(
         untracked_paths: &[&RepoPath],
-        tree: MergedTree,
+        tree: &MergedTree,
     ) -> String {
         let mut result = String::new();
         visit_collapsed_untracked_files(untracked_paths, tree, |path, is_dir| {
@@ -4713,7 +4713,7 @@ mod tests {
         ];
 
         insta::assert_snapshot!(
-            collect_collapsed_untracked_files_string(untracked, tracked),
+            collect_collapsed_untracked_files_string(untracked, &tracked),
             @"
         ? untracked_top_level_file
         ? dir/
