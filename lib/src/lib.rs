469bc606d6455bbd6912f5c841959ed8c125e37e
Martin von Zweigbergk <martinvonz@google.com>
2026-05-31 22:44:35 -0700

    repo: move `StoreFactories::default()` etc. to separate module
    This is high-level glue code that we want to remove from the `repo`
    module, so that module can be moved into a lower-level `jj-core`
    crate.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -41,6 +41,7 @@ pub mod conflicts;
 pub mod copies;
 pub mod dag_walk;
 pub mod dag_walk_async;
+pub mod default_backend_factories;
 pub mod default_index;
 pub mod default_submodule_store;
 pub mod diff;
