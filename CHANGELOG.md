56678d9e7961e097c30b59cb6fae5595aab75194
jlndr <jlinder@google.com>
2026-08-06 07:53:33 +0000

    cli: fix: add `-p/--patch` flag to display patch of changes per fixed commit.
    This adds `-p`/`--patch` flag to `fix` command to show patch of changes per commit. Fixes #9715.
    
    Example:
    ```
    $ jj --config ui.diff.format=git fix -p -s vyxlzqxr::mopmyzxr
    Fixed 3 commits of 3 checked.
    mopmyzxr bc7260bf another comment to fix
    diff --git a/cli/tests/test_fix_command.rs b/cli/tests/test_fix_command.rs
    index 5636280845..74e5fe7feb 100644
    --- a/cli/tests/test_fix_command.rs
    +++ b/cli/tests/test_fix_command.rs
    @@ -12,8 +12,8 @@
     // See the License for the specific language governing permissions and
     // limitations under the License.
    
    -fn unformatted_test_fn_2(   name: &str  , count: u32  ) -> String {
    -    let s  =  format!( "{name}_{count}" );
    +fn unformatted_test_fn_2(name: &str, count: u32) -> String {
    +    let s = format!("{name}_{count}");
         s
     }
    
    vyxlzqxr 50f927a4 test fix
    diff --git a/cli/src/commands/fix.rs b/cli/src/commands/fix.rs
    index 4f6ba84f1e..a1dd971a8c 100644
    --- a/cli/src/commands/fix.rs
    +++ b/cli/src/commands/fix.rs
    @@ -159,9 +159,9 @@
     /// ```
     /// The revisions are now all correctly formatted according to the
     /// configuration.
    -fn unformatted_test_fn_1(  a : usize , b: &str ) ->   bool {
    -    let x=1+2;
    -    x==3
    +fn unformatted_test_fn_1(a: usize, b: &str) -> bool {
    +    let x = 1 + 2;
    +    x == 3
     }
    
     #[derive(clap::Args, Clone, Debug)]
    ```

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,8 +19,8 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 * `jj bisect` will now mention when it cannot unambiguously find the first bad
   revision due to skips in evaluation.
 
-* `jj fix` now supports `--summary` and `--stat` flags to display modified
-  files or a histogram per fixed commit.
+* `jj fix` now supports `--summary`, `--stat` and `-p`/`--patch` flags to
+  display modified files, a histogram or a patch of changes per fixed commit.
 
 ### Fixed bugs
 
--- cli/tests/test_fix_command.rs
+++ cli/tests/test_fix_command.rs
@@ -2356,3 +2356,94 @@ fn test_stat_no_changes() {
     [EOF]
     "###);
 }
+
+#[test]
+fn test_patch() {
+    let test_env = TestEnvironment::default();
+    test_env.run_jj_in(".", ["git", "init", "repo"]).success();
+    let work_dir = test_env.work_dir("repo");
+    let formatter_path = assert_cmd::cargo::cargo_bin!("fake-formatter");
+    assert!(formatter_path.is_file());
+    let escaped_formatter_path = formatter_path.to_str().unwrap().replace('\\', r"\\");
+    test_env.add_config(format!(
+        r###"
+        [fix.tools.tool-1]
+        command = ["{formatter}", "--uppercase"]
+        patterns = ["file1"]
+
+        [fix.tools.tool-2]
+        command = ["{formatter}", "--append", "fixed"]
+        patterns = ["file2"]
+        "###,
+        formatter = escaped_formatter_path.as_str()
+    ));
+
+    work_dir.write_file("file1", "foo\nbar\n");
+    work_dir.write_file("file2", "baz\n");
+    work_dir.run_jj(["describe", "-m", "work item 1"]).success();
+    let output = work_dir.run_jj(["fix", "-p"]);
+    insta::assert_snapshot!(output, @r###"
+    ------- stderr -------
+    Fixed 1 commits of 1 checked.
+    qpvuntsm 17550c40 work item 1
+    Modified regular file file1:
+       1     : foo
+       2     : bar
+            1: FOO
+            2: BAR
+    Modified regular file file2:
+       1    1: baz
+            2: fixed
+    Working copy  (@) now at: qpvuntsm 17550c40 work item 1
+    Parent commit (@-)      : zzzzzzzz 00000000 (empty) (no description set)
+    Added 0 files, modified 2 files, removed 0 files
+    [EOF]
+    "###);
+}
+
+#[test]
+fn test_patch_multiple_commits() {
+    let test_env = TestEnvironment::default();
+    test_env.run_jj_in(".", ["git", "init", "repo"]).success();
+    let work_dir = test_env.work_dir("repo");
+    let formatter_path = assert_cmd::cargo::cargo_bin!("fake-formatter");
+    assert!(formatter_path.is_file());
+    let escaped_formatter_path = formatter_path.to_str().unwrap().replace('\\', r"\\");
+    test_env.add_config(format!(
+        r###"
+        [fix.tools.tool-1]
+        command = ["{formatter}", "--uppercase"]
+        patterns = ["file1"]
+
+        [fix.tools.tool-2]
+        command = ["{formatter}", "--append", "fixed"]
+        patterns = ["file2"]
+        "###,
+        formatter = escaped_formatter_path.as_str()
+    ));
+
+    work_dir.write_file("file1", "foo\nbar\n");
+    work_dir.run_jj(["describe", "-m", "work item 1"]).success();
+    work_dir.run_jj(["new"]).success();
+    work_dir.write_file("file2", "baz\n");
+    work_dir.run_jj(["describe", "-m", "work item 2"]).success();
+    let output = work_dir.run_jj(["fix", "-p"]);
+    insta::assert_snapshot!(output, @r###"
+    ------- stderr -------
+    Fixed 2 commits of 2 checked.
+    kkmpptxz 8331a050 work item 2
+    Modified regular file file2:
+       1    1: baz
+            2: fixed
+    qpvuntsm 3f596c8f work item 1
+    Modified regular file file1:
+       1     : foo
+       2     : bar
+            1: FOO
+            2: BAR
+    Working copy  (@) now at: kkmpptxz 8331a050 work item 2
+    Parent commit (@-)      : qpvuntsm 3f596c8f work item 1
+    Added 0 files, modified 2 files, removed 0 files
+    [EOF]
+    "###);
+}
