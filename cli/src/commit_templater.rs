f98742f3e5b95fa9f542a0708b2ea790def9f367
David Rieber <drieber@google.com>
2026-08-26 15:58:21 -0700

    WIP: jj independent workspaces prototype

--- cli/src/commit_templater.rs
+++ cli/src/commit_templater.rs
@@ -81,14 +81,15 @@ use jj_lib::signing::SigStatus;
 use jj_lib::signing::SignError;
 use jj_lib::signing::SignResult;
 use jj_lib::signing::Verification;
-use jj_lib::simple_workspace_store::SimpleWorkspaceStore;
+use jj_lib::simple_workspace_store::DefaultWorkspaceStoreFactory;
 use jj_lib::store::Store;
 use jj_lib::trailer;
 use jj_lib::trailer::Trailer;
 use jj_lib::ui_path::RepoPathUiConverter;
 use jj_lib::workspace::DefaultWorkspaceLoaderFactory;
 use jj_lib::workspace::WorkspaceLoaderFactory as _;
-use jj_lib::workspace_store::WorkspaceStore as _;
+use jj_lib::workspace_store::WorkspaceStoreFactory as _;
+use jj_lib::workspace_store::WorkspaceType;
 use once_cell::unsync::OnceCell;
 use pollster::FutureExt as _;
 use serde::Serialize as _;
@@ -1780,15 +1781,30 @@ impl WorkspaceRef {
         // decided which object should own the workspace store.
         let workspace_loader = DefaultWorkspaceLoaderFactory.create(base)?;
         let repo_path = workspace_loader.repo_path().to_owned();
-        let simple_workspace_store = SimpleWorkspaceStore::load(&repo_path)?;
+        let workspace_store = DefaultWorkspaceStoreFactory.load(&repo_path)?;
         // Workspaces created before jj 0.38.0 may not have a recorded path. List
         // templates should also keep rendering if a recorded path is stale or
         // unavailable. Use `jj workspace root --name` for strict path diagnostics.
-        let path = simple_workspace_store
+        let path = workspace_store
             .get_workspace_path(self.name())?
             .map(|workspace_path| repo_path.join(workspace_path))
             .and_then(|path| dunce::canonicalize(path).ok());
         Ok(path)
+    }
+
+    #[expect(unused)]
+    fn workspace_type(
+        &self,
+        path_converter: &RepoPathUiConverter,
+    ) -> Result<Option<WorkspaceType>, TemplatePropertyError> {
+        let RepoPathUiConverter::Fs { cwd: _, base } = path_converter;
+        // TODO: Stop reconstructing the workspace loader here once we've
+        // decided which object should own the workspace store.
+        let workspace_loader = DefaultWorkspaceLoaderFactory.create(base)?;
+        let repo_path = workspace_loader.repo_path().to_owned();
+        let workspace_store = DefaultWorkspaceStoreFactory.load(&repo_path)?;
+        let workspace_type = workspace_store.get_workspace_type(self.name())?;
+        Ok(workspace_type)
     }
 }
 
